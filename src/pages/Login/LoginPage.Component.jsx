9c0a8c038ba788baee749bdc10fad658168fcdd7
Akash <akashbaga011@gmail.com>
2018-06-13 18:30:23 +0300

    :
    This reverts commit 2a5965853c7e43d67deaa67515f28cc67877c6d6.

--- src/pages/Login/LoginPage.Component.jsx
+++ src/pages/Login/LoginPage.Component.jsx
@@ -31,17 +31,7 @@ class LoginPage extends React.Component {
     }
   }
 
-
   render() {
-    const styles = {
-      button: {
-        width: '14vw',
-        height: '5vh',
-        position: 'relative',
-        marginLeft: '2vw',
-
-      }
-    };
     const { from } = this.props.location.state || { from: { pathname: '/' } };
     setReferrerInlocationStorage(from.pathname);
     const referrer = getReferrerInlocationStorage();
@@ -53,36 +43,27 @@ class LoginPage extends React.Component {
       return <CircularProgressIndicator />;
     } else if (authenticateUser.isAuthenticated && hasToken) {
       return <Redirect to={(from.pathname = referrer)} />;
+    } else {
+      return (
+        <div className="login-page">
+          <div className="left-column">
+            <img className="landing-image" src="/assets/images/wire_landingpage.jpeg" alt="Wire Logo" />
+          </div>
+          <div className="right-column">
+            <div className="login-container">
+              <img className="landing-logo" src="/assets/images/wire_logo_landing.svg" />
+              <h2 className="title">Sign in with Andela email</h2>
+              <RaisedButton
+                className="button"
+                icon={<img className="google-logo" src="../../../assets/images/icons8-google.svg" />}
+                href={`${config.ANDELA_API_BASE_URL}/login?redirect_url=${config.BASE_URL}/login`}
+                label="login with google"
+              />
+            </div>
+          </div>
+        </div>
+      );
     }
-
-    return (
-      <div className="login-page">
-        <div className="left-container">
-          <div><img className="andela-logo" src="/assets/images/andelaLogo.png" /></div>
-          <div className="welcome-text">
-            <p>Welcome to <span className="wire">Wire </span><br />
-              Please sign in with your Google account to proceed</p>
-          </div>
-          <RaisedButton
-            className="button"
-            icon={<img className="google-logo" src="../../../assets/images/icons8-google.svg" />}
-            href={`${config.ANDELA_API_BASE_URL}/login?redirect_url=${config.BASE_URL}/login`}
-            label={<p className="label">Sign In With Google</p>}
-            style={styles.button}
-          />
-
-        </div>
-        <div className="right-container">
-          <img className="landing-image" src="/assets/images/wire_landing_page_vector@2x.png" />
-          <div className="right-text">
-            <p>An Incident <br />Reporting Platform</p>
-          </div>
-          <div className="underline" />
-        </div>
-
-
-      </div>
-    );
   }
 }
 
